6ca1736236f3462becd604618535b27f3aeaec78
Andrew Maximov <magcraft@gmail.com>
2015-03-22 00:53:29 +0300

    simple open file

---
+++ new_file.py
@@ -0,0 +1,10 @@
+# Use words.txt as the file name
+fname = raw_input("Enter file name: ")
+if len(fname) == 0:
+    fname = 'words.txt'
+fh = open(fname)
+for line in fh:
+    line = line.rstrip().upper()
+    print line
+
+
